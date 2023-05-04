bbb5982e06ee074344b6c9345942668b4011fc52
desmondBull88 <raiyanaziz88@gmail.com>
2023-05-03 20:25:15 -0400

    added responsiveness to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,20 @@ export default function Navbar() {
 
     return (
         <nav className='nav'>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/portfolio">Portfolio</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/contact">Contact </Link></li>
-            </ul>
-        </nav>
+            <div>
+                <a href='#' class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/contact">Contact </Link></li>
+                </ul>
+            </div>
+        </nav >
     )
 
 }
