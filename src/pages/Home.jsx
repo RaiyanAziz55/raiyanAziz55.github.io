18f690821e75a337542db5bd132ef5d65a036094
RaiyanAziz55 <raiyanaziz88@gmail.com>
2023-05-09 23:09:08 -0400

    added animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import './pageCss/App.css';
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import ReactDOM from 'react-dom';
 import Navbar from "../components/Navbar";
 import Image from './mockups/HomeBack4.jpg';
 import Image2 from './mockups/HomeBackground2.jpg';
-
+import { useInView } from 'react-intersection-observer'
 
 
 function Home() {
-
+  const { ref:imgRef, inView:imgView } = useInView();
   return (
     <body>
       <Navbar />
@@ -20,13 +21,13 @@ function Home() {
       <section className='contents'>
         <div className='inside'>
         <h1 id="slogan">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias sint aliquam voluptate atque nihil voluptatibus non voluptates est impedit, molestias laudantium quos tempora, explicabo magni officiis veniam dolorem cupiditate ullam.</h1>
-        <img src={Image2} alt="Girl in a jacket" id="img"></img>
+        <img src={Image2} ref={imgRef} className={imgView ? 'img fade-in is-visible':'img fade-in'}/>
         </div>
       </section>
-      <section className='section'>
+      <section className='section1'>
         <h1 className="category">PORTFOLIO</h1>
       </section>
-      <section className='section'>
+      <section className='section2'>
         <h1 className="category">SERVICES</h1>
       </section>
       </div>
