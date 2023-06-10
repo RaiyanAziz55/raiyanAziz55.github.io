c99ba01b5a229e1590384404a31aa3838d13b566
RaiyanAziz55 <raiyanaziz88@gmail.com>
2023-06-09 23:15:56 -0400

    update firestore

--- src/Hooks/useFirestore.jsx
+++ src/Hooks/useFirestore.jsx
@@ -22,19 +22,6 @@ const UseFirestore = () => {
   };
 
   useEffect(() => {
-    const getImgList = async () => {
-        try {
-          const data = await getDocs(collectionRef);
-          const filteredData = data.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setImgs(filteredData);
-          console.log(imgs);
-        } catch (err) {
-          console.error(err);
-        }
-      };
     getImgList();
   }, []);
 
