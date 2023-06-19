5af4f059c6e725225777cd301a5eac957c98c217
RaiyanAziz55 <raiyanaziz88@gmail.com>
2023-06-19 00:40:25 -0400

    added highlsight effects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -17,18 +17,30 @@ function Portfolio() {
     </div>
   <div className='grids'>
       <div className='folders'>
-        <div className='FFH'>
-          <img src={img1} alt="ffh" className='section' />
-          <text className='t1'> FFH </text>
+        <div className='FFH'
+                onMouseEnter={() => setIsHighlight(true)}
+                onMouseLeave={() => setIsHighlight(false)}
+        >
+          <img src={img1} alt="ffh" className={isHighlight ?  'section active':'section'} />
+          <div className={isHighlight ?  'layer1 active':'layer1'}></div>
+          <text className={isHighlight ?  't1 active':'t1'}> FFH </text>
           
         </div>
-        <div className='Unlucciest'>
+        <div className='Unlucciest'
+            onMouseEnter={() => setIsHighlight(true)}
+            onMouseLeave={() => setIsHighlight(false)}
+        >
         <img src={img2} alt="unluck" className='section'/>
-        <text className='t1'> UNLUCCIEST </text>
+        <div className={isHighlight ?  'layer1 active':'layer1'}></div>
+        <text className={isHighlight ?  't1 active':'t1'}> UNLUCCIEST </text>
         </div>
-        <div className='personal'>
+        <div className='personal'
+         onMouseEnter={() => setIsHighlight(true)}
+         onMouseLeave={() => setIsHighlight(false)}
+        >
         <img src={img3} alt="ffh" className='section' />
-        <text className='t1'> PERSONAL </text>
+        <div className={isHighlight ?  'layer1 active':'layer1'}></div>
+        <text className={isHighlight ?  't1 active':'t1'}> PERSONAL </text>
         </div>
       </div>
       </div>
